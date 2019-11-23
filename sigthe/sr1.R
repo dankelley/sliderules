cde01739ee0966e49a0d619db261920790f25be5
dankelley <kelley.dan@gmail.com>
2019-11-23 16:53:13 -0400

    .

--- sigthe/sr1.R
+++ sigthe/sr1.R
@@ -6,26 +6,27 @@ load("00.rda")
 load("02.rda")
 S <- seq(S0, Smax, length.out=n)
 T <- seq(T0, Tmax, length.out=n)
-par(mar=c(3, 1, 1, 1))
+par(mar=c(3, 1, 3, 1))
 plot(S, seq(0, 1, length.out=n), xlab="", ylab="", type="n", axes=draft, yaxs="i", xaxs="i")
-if (draft)
-    box(col=2)
-Sbig <- seq(S0, Smax, by=1)
-Smiddle <- seq(S0, Smax, by=0.5)
-Ssmall <- seq(S0, Smax, by=0.1)
-tclSmall <- 0.15 * if (side==1) -1 else 1
-tclMiddle <- 2 * tclSmall
-tclBig <- 2 * tclMiddle
-labels <- seq(S0, Smax, by=1)
-par(xpd=NA)
-rug(Ssmall, tcl=tclSmall, side=side)
-rug(Smiddle, tcl=tclMiddle, side=side)
-rug(Sbig, tcl=tclBig, side=side)
-if (side == 1) {
-    text(Sbig, 0.3 * tclSmall + rep(0, length.out=length(Sbig)), Sbig)
-} else {
-    text(Sbig, -0.3 * tclSmall + rep(1, length.out=length(Sbig)), Sbig)
+myrug <- function(x, tcl, y)
+{
+    segments(x, y, x, y+tcl)
 }
-##lines(c(S0, Smax), rep(if (side==1) 0 else 1, 2), col="lightgray")
-side
+fancyAxis <- function(xSmall, xMiddle, xBig, tclSmall, tclMiddle, tclBig, side=1, y=0)
+{
+    if (missing(tclSmall)) tclSmall <- 0.01 * if (side==1) -1 else 1
+    if (missing(tclMiddle)) tclMiddle <- 2 * tclSmall
+    if (missing(tclBig)) tclBig <- sqrt(2) * tclMiddle
+    labels <- seq(S0, Smax, by=1)
+    par(xpd=NA)
+    myrug(xSmall, tcl=tclSmall, y=y)
+    myrug(xMiddle, tcl=tclMiddle, y=y)
+    myrug(xBig, tcl=tclBig, y=y)
+    text(xBig, 2*tclBig + rep(y, length.out=length(xBig)), xBig)
+}
+SBig <- seq(S0, Smax, by=1)
+SMiddle <- seq(S0, Smax, by=0.5)
+SSmall <- seq(S0, Smax, by=0.1)
+fancyAxis(SSmall, SMiddle, SBig, side=1, y=0.5)
+fancyAxis(SSmall, SMiddle, SBig, side=3, y=1)
 
