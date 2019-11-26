dc6822b65b2afb20932b722810bdda4fa635d4dc
dankelley <kelley.dan@gmail.com>
2019-11-25 20:02:01 -0400

    start work on sigthe circular +
    1. T broken
    2. nothing checked
    3. uncertainties not printed
    4. no "for CTT by DEK" label
    5. no serial-number label

---
+++ sigthe/sigma_theta_circular_1.R
@@ -0,0 +1,150 @@
+library(oce)
+debug <- 1
+cexName <- 0.9
+load("00.rda")
+load("04.rda")
+C <- coef(m)
+Sfunc <- function(S)
+{
+    C["(Intercept)"] + C["SS"]*(S-S0) + C["SS2"]*(S-S0)^2
+}
+Tfunc <- function(T)
+{
+    Sfunc(Smax) + C["TT"]*(T-T0) + C["TT2"]*(T-T0)^2
+}
+
+if (!interactive()) pdf("sigma_theta_circular_1.pdf", width=7, height=7, pointsize=8)
+nseg <- 64
+xc <- 0
+yc <- 0
+R <- list(sigthe=0.65, S=0.8, T=0.81)
+scale <- 5 * 2 * pi
+par(mar=rep(1, 4))
+sigthe0 <- floor(swSigmaTheta(min(G$S), max(G$T), 100))
+sigthemax <- ceiling(swSigmaTheta(max(G$S), min(G$T), 100))
+plot(c(-1, 1), c(-1, 1), xlab="", ylab="", type="n", axes=debug>0)# , yaxs="i", xaxs="i")
+if (debug)
+    grid(col="pink", lty=1)
+
+circle <- function(R, ...)
+{
+    theta <- seq(0, 2*pi, length.out=nseg)
+    lines(xc + R * cos(theta), yc + R * sin(theta), ...)
+}
+
+myrugCircular <- function(x, tcl, R, inside=TRUE)
+{
+    if (debug > 1)
+        cat("  in myrugCircular(x=c(", x[1], ",", x[2], ",...,", tail(x,1), "), tcl=", tcl, ", R=", R, ")\n")
+    oldxpd <- par("xpd")
+    par(xpd=NA)
+    tcl <- abs(tcl) * if (inside) -1 else 1
+    x0 <- xc + R * cos(x*pi/180)
+    y0 <- yc + R * sin(x*pi/180)
+    x1 <- xc + (R + tcl) * cos(x*pi/180)
+    y1 <- yc + (R + tcl) * sin(x*pi/180)
+    segments(x0, y0, x1, y1)
+    par(xpd=oldxpd)
+}
+
+fancyAxisCircular <- function(xSmall, xMiddle, xBig, func, tclSmall, tclMiddle, tclBig, inside=TRUE, R=1)
+{
+    if (missing(func)) func <- function(x) x
+    if (missing(tclSmall)) tclSmall <- 0.01 * if (inside) 1 else -1
+    if (missing(tclMiddle)) tclMiddle <- 2 * tclSmall
+    if (missing(tclBig)) tclBig <- sqrt(2) * tclMiddle
+    labels <- xBig
+    if (debug > 0) {
+        cat("in fancyAxisCircular(R=", R, ")\n")
+        cat("  ", vectorShow(labels))
+        cat("  ", vectorShow(xSmall))
+        cat("  ", vectorShow(xMiddle))
+        cat("  ", vectorShow(xBig))
+    }
+    oldxpd <- par("xpd")
+    par(xpd=NA)
+    myrugCircular(scale * func(xSmall), tcl=tclSmall, inside=inside, R=R)
+    myrugCircular(scale * func(xMiddle), tcl=tclMiddle, inside=inside, R=R)
+    myrugCircular(scale * func(xBig), tcl=tclBig, inside=inside, R=R)
+    RR <- if (inside) R-2.25*tclBig else R+2.25*tclBig
+    text(xc + RR * cos(scale*pi/180*func(xBig)),
+         yc + RR * sin(scale*pi/180*func(xBig)), xBig)
+    ##2.5*tclBig + scale*pi/180*rep(R, length.out=length(xBig))), xBig)
+    circle(R=R)
+    par(xpd=oldxpd)
+}
+
+## S axis
+SBig <- seq(S0, Smax, by=1)
+SMiddle <- seq(S0, Smax, by=0.5)
+SSmall <- seq(S0, Smax, by=0.1)
+fancyAxisCircular(SSmall, SMiddle, SBig, func=Sfunc, inside=TRUE, R=R$S)
+text(Sfunc(min(G$S)-1.0), 0.69, expression("Practical Salinity"), pos=1, cex=1)#cexName)
+#text(Sfunc(min(SBig))+0.4, 0.69, expression("S"), pos=1, cex=cexName)
+
+TBig <- seq(T0, Tmax, by=2)
+TMiddle <- seq(T0, Tmax, by=1)
+TSmall <- seq(T0, Tmax, by=0.5)
+fancyAxisCircular(TSmall, TMiddle, TBig, func=Tfunc, inside=TRUE, R=R$T)
+oldxpd <- par("xpd")
+par(xpd=NA)
+text(Tfunc(max(TBig)+4), 1.130, expression("In-situ Temperature ["*degree*"C]"), pos=1, cex=1)#cexName)
+#text(Tfunc(max(TBig)-1), 1.125, expression("T ["*degree*"C]"), pos=1, cex=cexName)
+#text(Tfunc(max(TBig)-1), 1.125, expression("T"), pos=1, cex=cexName)
+par(xpd=oldxpd)
+
+
+## sigma-theta axis
+sigtheBig <- seq(sigthe0, sigthemax, by=1)
+sigtheMiddle <- seq(sigthe0, sigthemax, by=0.5)
+sigtheSmall <- seq(sigthe0, sigthemax, by=0.1)
+fancyAxisCircular(sigtheSmall, sigtheMiddle, sigtheBig, inside=TRUE, R=R$sigthe)
+text(min(sigtheSmall)+0.49, 0.14, expression(sigma[theta]*" ["*kg/m^3*"]"), pos=1, cex=cexName)
+circle(R=1, col=2)
+
+y0 <- 0.25
+dy <- 0.08
+x <- sigthe0 - 0.1
+##cexText <- 0.935
+cexText <- 0.95
+text(-0.26, 0.42, expression("Seawater "*sigma[theta]*" Calculator"), pos=4, cex=1.4*cexText)
+text(-0.25, 0.35, expression("(1) Place T=0 above observed S."), pos=4, cex=cexText)
+text(-0.24, 0.30, expression("(2) Move pointer to observed T."), pos=4, cex=cexText)
+text(-0.33, 0.25, expression("(3) Read approximate "*sigma[theta]*" from inner ring."), pos=4, cex=cexText)
+text(-0.25, 0.20, expression("(4) Add "*sigma[theta]*" correction from graph."), pos=4, cex=cexText)
+##text(sigthe0, D0, sprintf("E.G. %.2f kg/m^3 at S=%.0f, T=%.0fC", swSigmaTheta(35, 0, 0), 35, 20, 0), pos=4)
+EGS <- 32
+EGT <- 5
+EGp <- 0
+EGsigma <- sprintf("%.2f", round(swSigmaTheta(EGS, EGT, EGp), 3))
+text(-0.38, 0.15, bquote("Example: "*sigma[theta]*"="*.(EGsigma)*kg/m^3*" at S="*.(EGS)*" and T="*.(EGT)*degree*"C."), pos=4, cex=cexText)
+ERRrms <- round(RMS(residuals(m)), 2)
+ERRmax <- round(max(residuals(m)), 2)
+text(x, y0, bquote("Error: "*.(ERRrms)*kg/m^3*" (rms), "*.(ERRmax)*kg/m^3*" (max) from 0 to 500dbar."), pos=4, cex=cexText)
+
+text(0.08, 0.58, expression(sigma[theta]*" ["* kg/m^3*"]"), cex=1, srt=-10)
+text(0.15, 0.72, expression("Salinity"), cex=1, srt=-10)
+
+omar <- par("mar")
+par(new=TRUE)
+#par(mar=omar+c(3,38,5,1), mgp=c(0.7, 0.2, 0), cex=0.6, tcl=-0.25)
+par(mai=c(2.4,2.8,3.2,2.8), cex=0.9, tcl=-0.25, mgp=c(1.3, 0.3, 0))
+range <- range((G$S - Smid)*(G$T - Tmid))
+x <- seq(range[1], range[2], length.out=n)
+plot(x, C["SSTT"]*x, lwd=2, xaxs="i",
+     xlab=paste("(S-", Smid, ")*(T-", Tmid, ")", sep=""), ylab=expression(kg/m^3), type="l")
+grid(lty=1, col="lightgray")
+rug(side=2, x=seq(-0.1,0.1,by=0.01), ticksize=-0.02)
+legend("topright", legend=expression("Add to "*sigma[theta]), bg="white")
+lines(x, C["SSTT"]*x, lwd=2)
+## ## regression check
+## if (debug > 0) {
+##     par(xpd=FALSE)
+##     abline(v=swSigmaTheta(31,T0,0), col=2)
+##     abline(v=24.58, col=2, lty=2)
+##     mtext("error 0.1 kg/m^3", side=3, at=24.58, col=2)
+##     par(xpd=NA)
+## }
+
+if (!interactive()) dev.off()
+
