e8251f515bed5c32b0bf4d9f1ef005368b2426c3
dankelley <kelley.dan@gmail.com>
2020-12-17 14:42:07 -0400

    cleanup

--- geostrophy/geostrophy_circular_1.R
+++ geostrophy/geostrophy_circular_1.R
@@ -21,19 +21,25 @@ library(oce)
 debug <- 0                             # set to 1 if adjusting central text (to centre by eye)
 g <- 9.8
 L <- 100e3                             # eta varies over this distance [m]
-R <- list(sigthe=0.65, S=0.8, lat=0.81)  # radii of axis circles
+R <- list(v=0.65, eta=0.8, lat=0.81)   # radii of axis circles
 col <- list(sigmaTheta="royalblue", lat="darkred", S="darkgreen")
-scale <- 2.0 * 2 * pi                        # adjust this to fill most of circle
+## Adjust offset and scale to fill space nicely
+offset <- 0
+scale <- 2.5 * 2 * pi                  # adjust this to fill most of circle
+
 cexName <- 1.25                        # cex for axis names
+
+RMS <- function(x)
+    sqrt(mean(x^2, na.rm=TRUE))
 
 latfunc <- function(latitude)
 {
-    -scale*(log10(g/(L*oce::coriolis(latitude))))
+    offset - scale*(log10(g/(L*oce::coriolis(latitude))))
 }
 
 etafunc <- function(eta)
 {
-    scale * log10(eta)
+    offset + scale * log10(eta)
 }
 message("latfunc(latitude=c(5,90)): ", paste(round(latfunc(c(5,90)),2), collapse=" "))
 message("etafunc(eta=c(0.02,2)):    ", paste(round(etafunc(c(0.02,2)),2), collapse=" "))
@@ -109,9 +115,11 @@ fancyAxisCircular <- function(xSmall, xMiddle, xBig, func, tclSmall, tclMiddle, 
     myrugCircular(scale * func(xMiddle), tcl=tclMiddle, inside=inside, R=R, col=col, debug=debug)
     myrugCircular(scale * func(xBig), tcl=tclBig, inside=inside, R=R, col=col, debug=debug)
     theta <- scale * pi / 180 * func(xBig) # location along circumferenace
+    ok <- is.finite(theta)
     for (i in seq_along(theta)) {
-        text((R-2.5*tclBig) * cos(theta[i]), (R-2.5*tclBig) * sin(theta[i]), xBig[i],
-             srt=-90+theta[i]*180/pi, col=col)
+        if (ok[i])
+            text((R-2.5*tclBig) * cos(theta[i]), (R-2.5*tclBig) * sin(theta[i]), xBig[i],
+                 srt=-90+theta[i]*180/pi, col=col)
     }
     circle(R=R, col=col)
     par(xpd=oldxpd)
@@ -119,10 +127,14 @@ fancyAxisCircular <- function(xSmall, xMiddle, xBig, func, tclSmall, tclMiddle, 
 
 lat0 <- 10
 latmax <- 90
+eta0 <- 0
+etamax <- 2.0
+v0 <- 0
+vmax <- 3
 
-for (layer in c("lower", "upper", "pointer")[1]) {
+for (layer in c("lower", "upper", "pointer")) {
     if (!interactive())
-        pdf(paste("sigma_theta_circular_1_", layer, ".pdf", sep=""), width=7, height=7, pointsize=8)
+        pdf(paste("geostrophy_circular_1_", layer, ".pdf", sep=""), width=7, height=7, pointsize=8)
     par(mar=rep(1, 4), lwd=1.4)
     plot(c(-1, 1), c(-1, 1), xlab="", ylab="", type="n", axes=debug>0)# , yaxs="i", xaxs="i")
     if (debug)
@@ -138,31 +150,31 @@ for (layer in c("lower", "upper", "pointer")[1]) {
         text(0.472, R$lat-0.058, expression(degree*"N"), pos=1, cex=cexName, srt=-32, col=col$lat)
         circle(R=R$lat+0.15, col="gray", lty="dotted")
     } else if (layer == "upper") {
-        ## S axis
-        SBig <- seq(S0, Smax, by=1)
-        SMiddle <- seq(S0, Smax, by=0.5)
-        SSmall <- seq(S0, Smax, by=0.1)
-        fancyAxisCircular(SSmall, SMiddle, SBig, func=Sfunc, inside=TRUE, R=R$S, col=col$S, debug=debug)
-        circularText(R$S-0.02, 82, -2, "S\\:al\\!inity", cex=cexName, col=col$S)
+        ## eta axis
+        etaBig <- seq(eta0, etamax, by=0.2)
+        etaMiddle <- seq(eta0, etamax, by=0.1)
+        etaSmall <- seq(eta0, etamax, by=0.05)
+        fancyAxisCircular(etaSmall, etaMiddle, etaBig, func=etafunc, inside=TRUE, R=R$eta, col=col$eta, debug=debug)
+        circularText(R$eta-0.02, 82, -2, "Eta", cex=cexName, col=col$S)
         ##
-        ## sigma-theta axis
-        sigtheBig <- seq(sigthe0, sigthemax, by=1)
-        sigtheMiddle <- seq(sigthe0, sigthemax, by=0.5)
-        sigtheSmall <- seq(sigthe0, sigthemax, by=0.1)
-        fancyAxisCircular(sigtheSmall, sigtheMiddle, sigtheBig, inside=TRUE, R=R$sigthe, col=col$sigmaTheta, debug=debug)
-        text(0.000, R$sigthe-0.070, expression(sigma[theta]), cex=1.2*cexName, srt=-4, col=col$sigmaTheta)
-        text(0.040, R$sigthe-0.068, expression(" ["), cex=cexName, srt=-5, col=col$sigmaTheta)
-        text(0.080, R$sigthe-0.073, "kg", cex=cexName, srt=-10, col=col$sigmaTheta)
-        text(0.118, R$sigthe-0.076, "/", cex=cexName, srt=-14, col=col$sigmaTheta)
-        text(0.153, R$sigthe-0.077, expression(" "*m^3), cex=cexName, srt=-17, col=col$sigmaTheta)
-        text(0.190, R$sigthe-0.100, "]", cex=cexName, srt=-20, col=col$sigmaTheta)
+        ## v axis
+        vBig <- seq(v0, vmax, by=1)
+        vMiddle <- seq(v0, vmax, by=0.5)
+        vSmall <- seq(v0, vmax, by=0.1)
+        fancyAxisCircular(vSmall, vMiddle, vBig, inside=TRUE, R=R$v, col=col$sigmaTheta, debug=debug)
+        text(0.000, R$v-0.070, expression(v*" [m/s]"), cex=1.2*cexName, srt=-4, col=col$sigmaTheta)
+        ## text(0.040, R$sigthe-0.068, expression(" ["), cex=cexName, srt=-5, col=col$sigmaTheta)
+        ## text(0.080, R$sigthe-0.073, "kg", cex=cexName, srt=-10, col=col$sigmaTheta)
+        ## text(0.118, R$sigthe-0.076, "/", cex=cexName, srt=-14, col=col$sigmaTheta)
+        ## text(0.153, R$sigthe-0.077, expression(" "*m^3), cex=cexName, srt=-17, col=col$sigmaTheta)
+        ## text(0.190, R$sigthe-0.100, "]", cex=cexName, srt=-20, col=col$sigmaTheta)
         y0 <- 0.25
         dy <- 0.08
-        x <- sigthe0 - 0.1
+        x <- v0 - 0.1
         cexText <- 0.95
         y <- 0.45
         dy <- 0.05
-        text(-0.27, y, expression("Seawater "*sigma[theta]*" Calculator"), pos=4, cex=1.4*cexText)
+        text(-0.27, y, expression("Geostrophic Speed Calculator"), pos=4, cex=1.4*cexText)
         y <- y - 1.2 * dy
         text(-0.32, y, expression("(1) Set 0"*degree*"C to be at observed salinity,"), pos=4, cex=cexText)
         y <- y - dy
@@ -178,34 +190,34 @@ for (layer in c("lower", "upper", "pointer")[1]) {
         EGsigma <- sprintf("%.2f", round(swSigmaTheta(EGS, EGT, EGp), 3))
         text(-0.375, y, bquote("Example: "*sigma[theta]*"="*.(EGsigma)*kg/m^3*" at S="*.(EGS)*" and T="*.(EGT)*degree*"C."), pos=4, cex=cexText)
         y <- y - dy
-        ERRrms <- round(RMS(residuals(m)), 2)
-        ERRmax <- round(max(residuals(m)), 2)
-        text(-0.545, y, bquote("Accurate to "*.(ERRrms)*kg/m^3*" (rms) and "*.(ERRmax)*kg/m^3*" (max) up to 500 dbar."), pos=4, cex=cexText)
+        #ERRrms <- round(RMS(residuals(m)), 2)
+        #ERRmax <- round(max(residuals(m)), 2)
+        #text(-0.545, y, bquote("Accurate to "*.(ERRrms)*kg/m^3*" (rms) and "*.(ERRmax)*kg/m^3*" (max) up to 500 dbar."), pos=4, cex=cexText)
         y <- y - dy
-        text(-0.48, y, "Ser. No. 1, for CTT", pos=4, cex=cexText, font=2)
-        text(+0.15, y, "(c) 2019 Dan Kelley", pos=4, cex=cexText)
+        text(-0.48, y, "Ser. No. 1, for xxx", pos=4, cex=cexText, font=2)
+        text(+0.15, y, "(c) 2020 Dan Kelley", pos=4, cex=cexText)
         circle(R=R$S+0.01, col="gray", lty="dotted")
-        omar <- par("mar")
-        par(new=TRUE)
-        par(mai=c(2.2,2.8,3.8,2.8), cex=0.9, tcl=-0.25, mgp=c(1.3, 0.3, 0))
-        range <- range((G$S - Smid)*(G$T - Tmid))
-        x <- seq(range[1], range[2], length.out=n)
-        plot(x, C["SSTT"]*x, lwd=1.4, xaxs="i", ylim=c(-0.1, 0.1),
-             xlab=paste("(S-", Smid, ")*(T-", Tmid, ")", sep=""), ylab=expression(kg/m^3), type="l")
-        mtext("0.00", side=2, at=0, line=0.3, cex=0.9, col=col$sigmaTheta)
-        grid(lty=1, col="lightgray")
-        rug(side=2, x=seq(-0.1,0.1,by=0.01), ticksize=-0.02, col=col$sigmaTheta)
-        legend("topright", legend=expression("Add to "*sigma[theta]), bg="white")
-        lines(x, C["SSTT"]*x, lwd=1.4)
+        ##> omar <- par("mar")
+        ##> par(new=TRUE)
+        ##> par(mai=c(2.2,2.8,3.8,2.8), cex=0.9, tcl=-0.25, mgp=c(1.3, 0.3, 0))
+        ##> range <- range((G$S - Smid)*(G$T - Tmid))
+        ##> x <- seq(range[1], range[2], length.out=n)
+        ##> plot(x, C["SSTT"]*x, lwd=1.4, xaxs="i", ylim=c(-0.1, 0.1),
+        ##>      xlab=paste("(S-", Smid, ")*(T-", Tmid, ")", sep=""), ylab=expression(kg/m^3), type="l")
+        ##> mtext("0.00", side=2, at=0, line=0.3, cex=0.9, col=col$sigmaTheta)
+        ##> grid(lty=1, col="lightgray")
+        ##> rug(side=2, x=seq(-0.1,0.1,by=0.01), ticksize=-0.02, col=col$sigmaTheta)
+        ##> legend("topright", legend=expression("Add to "*sigma[theta]), bg="white")
+        ##> lines(x, C["SSTT"]*x, lwd=1.4)
     } else if (layer == "pointer") {
-        circle(R=R$T+0.15, col="gray", lty="dotted")
+        circle(R=R$lat+0.15, col="gray", lty="dotted")
         pointerWidth <- 0.1
-        lines(rep(pointerWidth, 2), c(0, R$T+0.15-pointerWidth))
-        lines(rep(-pointerWidth, 2), c(0, R$T+0.15-pointerWidth))
+        lines(rep(pointerWidth, 2), c(0, R$lat+0.15-pointerWidth))
+        lines(rep(-pointerWidth, 2), c(0, R$lat+0.15-pointerWidth))
         theta <-seq(-pi, 0, length.out=128)
         pointerRadius <- pointerWidth
         lines(pointerRadius*cos(theta), pointerRadius*sin(theta))
-        lines(pointerRadius*cos(-theta), R$T+0.15-pointerWidth+pointerRadius*sin(-theta))
+        lines(pointerRadius*cos(-theta), R$lat+0.15-pointerWidth+pointerRadius*sin(-theta))
     } else {
         stop("unrecognized 'layer' (programming error)")
     }
