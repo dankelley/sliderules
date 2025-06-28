4a793e3c829ca2892dd44ee54e7de908c1379dc3
dankelley <kelley.dan@gmail.com>
2025-06-28 09:41:34 -0300

    Steppingstone: non-pretty spice ticks

--- spice/02.R
+++ spice/02.R
@@ -16,29 +16,32 @@ fSA <- function(SA) {
 fCT <- function(CT) {
     -2.557009e+01 + CT * (5.396252e-02 + CT * (6.762300e-03 - 5.595578e-05 * CT))
 }
-spiciness <- function(SA, CT) {
+fspice <- function(SA, CT) {
     fSA(SA) + fCT(CT)
 }
 error <- function(SA, CT) {
-    gsw_spiciness0(SA, CT) - spiciness(SA, CT)
+    gsw_spiciness0(SA, CT) - (fSA(SA) + fCT(CT))
 }
-error(30, 10)
+message("Error in a test: ", error(30, 10))
 
 if (!interactive()) png("02.png", unit = "in", width = 7, height = 7, res = 500)
-par(mar = c(0, 2, 0, 2))
-lim <- c(-0.025, 1.025)
-plot(lim, lim, type = "n", axes = FALSE, xlab = "", ylab = "")
-points(0.5, 0.5)
+par(mar = c(0, 2, 1, 2))
+pagelim <- c(-0.025, 1.025)
+plot(pagelim, pagelim, type = "n", axes = FALSE, xlab = "", ylab = "")
 lines(c(0, 0), c(0, 1))
 lines(c(0.5, 0.5), c(0, 1))
 lines(c(1, 1), c(0, 1))
 
-SAlim <- c(28, 36)
+SAlim <- c(28, 35)
 CTlim <- c(0, 25)
 Alim <- fSA(SAlim)
 Clim <- fCT(CTlim)
+Blim <- Alim + Clim # works for additive formulae
 SAfcn <- function(SA) (fSA(SA) - Alim[1]) / (Alim[2] - Alim[1])
+SAfcnInverse <- function(eta) SAlim[1] + eta * (SAlim[2] - SAlim[1])
 CTfcn <- function(CT) (fCT(CT) - Clim[1]) / (Clim[2] - Clim[1])
+CTfcnInverse <- function(eta) CTlim[1] + eta * (CTlim[2] - CTlim[1])
+spicefcn <- function(spice) (fspice(spice) - Blim[1]) / (Blim[2] - Blim[1])
 n <- 10
 SApretty <- pretty(SAlim, n = n)
 SApretty <- SApretty[SAlim[1] <= SApretty & SApretty <= SAlim[2]]
@@ -72,17 +75,22 @@ text(1 + eps, 0.5, expression("Conservative Temperature [" * degree * "C]"), srt
 # be to use horizontal lines but that requires an inverse
 # fcn from e.g. page-to-SA (and we only SA-to-page defined
 # so far)
-text(0.5, 1 + eps / 4, "Spiciness [g/kg]")
-# ? spicelim <- spiciness(SAlim, CTlim)
-# ? spicePretty
-# ? nspice <- length(spicelim)
-# ? for (i in seq_len(nspice)) {
-# ?     lines(c(0.5, 0.5 + D), rep(CTfcn(CTpretty[i]), 2))
-# ?     text(1 + 0.3 * eps, CTfcn(CTpretty[i]), sprintf("%5.0f", CTpretty[i]))
-# ? }
-# ? dCT <- diff(CTpretty[1:2]) / 2
-# ? for (CT in seq(CTpretty[1], tail(CTpretty, 1), dCT)) {
-# ?     lines(c(1, 1 + gamma * D), rep(CTfcn(CT), 2))
-# ? }
+text(0.5, 1 + eps / 2, "Spiciness0 [g/kg]")
+spicelim <- gsw_spiciness0(SAlim, CTlim)
+message(oce::vectorShow(spicelim))
+f <- 0
+for (f in seq(0, 1, 0.1)) {
+    SA <- SAfcnInverse(f)
+    CT <- CTfcnInverse(f)
+    spice <- gsw_spiciness0(SA, CT)
+    message(sprintf("f=%.2f -> spice=%.2f", f, spice))
+    lines(c(0.5, 0.5 + tickLong), rep(f, 2), lwd = lwdTickLong)
+    text(0.5 + tickLong, f, sprintf("%.2f", spice), pos = 4)
+}
+for (f in seq(0, 1, 0.05)) {
+    lines(c(0.5, 0.5 + tickShort), rep(f, 2), lwd = lwdTickShort)
+}
+text(0.1, 0.98, sprintf("check spice(35,25) = %.2f", gsw_spiciness0(35, 25)), pos = 4)
+text(0.1, 0.02, sprintf("check spice(28,0)  = %.2f", gsw_spiciness0(28, 0)), pos = 4)
 
 if (!interactive()) dev.off()
